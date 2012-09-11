3af6522f2da5fbfea8e7b32a5d6d50b4610de3b3
Adam Beynon <adam.beynon@gmail.com>
2012-09-11 11:58:13 +0100

    Basic specs for Event class

--- lib/opal-jquery/event.rb
+++ lib/opal-jquery/event.rb
@@ -1,3 +1,21 @@
 class Event < `$.Event`
-  
+  def current_target
+    %x{
+      return $(#{self}.currentTarget);
+    }
+  end
+
+  def target
+    %x{
+      if (#{self}._opalTarget) {
+        return #{self}._opalTarget;
+      }
+      
+      return #{self}._opalTarget = $(#{self}.target);
+    }
+  end
+
+  def type
+    `#{self}.type`
+  end
 end
---
+++ spec/event_spec.rb
@@ -0,0 +1,66 @@
+describe Event do
+  before do
+    @div = Document.parse <<-HTML
+      <div id="on-spec">
+        <div id="foo">
+          <div id="bar"></div>
+        </div>
+        <div id="baz"></div>
+      </div>
+    HTML
+
+    @div.append_to_body
+  end
+
+  after do
+    @div.remove
+  end
+
+  it '#current_target returns the current element in the bubbling' do
+    foo = Document['#foo']
+    bar = Document['#bar']
+    result = []
+
+    foo.on(:click) { |e| result << e.current_target.id }
+    bar.on(:click) { |e| result << e.current_target.id }
+
+    foo.trigger(:click)
+    result.should == ['foo']
+    result = []
+
+    bar.trigger(:click)
+    result.should == ['bar', 'foo']
+  end
+
+  it '#type returns the type of event' do
+    type = nil
+    foo  = Document['#foo']
+
+    foo.on(:click) { |e| type = e.type }
+    foo.on(:mousedown) { |e| type = e.type }
+    foo.on(:opal_random) { |e| type = e.type }
+
+    foo.trigger(:click)
+    type.should == :click
+
+    foo.trigger(:mousedown)
+    type.should == :mousedown
+
+    foo.trigger(:opal_random)
+    type.should == :opal_random
+  end
+
+  it '#target returns a JQuery wrapper around the element that triggered the event' do
+    foo = Document['#foo']
+    bar = Document['#bar']
+    target = nil
+
+    foo.on(:click) { |e| target = e.target.id }
+
+    foo.trigger(:click)
+    target.should == 'foo'
+
+    bar.trigger(:click)
+    target.should == 'bar'
+  end
+end
