d3144d2d8b6e9f2a67d6280649f37d61f611c675
Adam Beynon <adam.beynon@gmail.com>
2012-09-12 13:06:45 +0100

    Add HTTP specs for #callback, #errback, #json, #ok?

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -7,6 +7,32 @@ describe HTTP do
     end
   end
 
+  describe '#callback' do
+    async 'can add a success callback after the request is sent' do
+      http = HTTP.get('data/simple.txt')
+      http.callback do |response|
+        run_async { response.ok?.should be_true }
+      end
+    end
+  end
+
+  describe '#callback' do
+    async 'can add a failure callback after the request is sent' do
+      http = HTTP.get('data/bad_url.txt')
+      http.errback do |response|
+        run_async { response.ok?.should be_false }
+      end
+    end
+  end
+
+  describe '#json' do
+    async 'returns the json converted into native ruby objects' do
+      HTTP.get('data/user.json') do |response|
+        run_async { response.json.should == { 'name' => 'Adam', 'age' => 26 } }
+      end
+    end
+  end
+
   describe '#ok?' do
     async 'returns true when the request was a sucess' do
       HTTP.get('data/simple.txt') do |response|
