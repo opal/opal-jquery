da64dc1dd7553e5e081e4255d7a8242dbbe1556d
Adam Beynon <adam.beynon@gmail.com>
2013-02-08 15:19:43 +0000

    Use ruby-style require statements in source

---
+++ lib/assets/javascripts/opal/jquery.rb
@@ -0,0 +1,6 @@
+require 'opal/jquery/document'
+require 'opal/jquery/element'
+require 'opal/jquery/event'
+require 'opal/jquery/http'
+require 'opal/jquery/kernel'
+require 'opal/jquery/local_storage'
