acbe323eeb569d71134aa6cec787a69d3532be87
Adam Beynon <adam.beynon@gmail.com>
2013-07-11 20:02:40 +0100

    Add Document.title and Document.title=
    These are nice(r) shortcuts for reading/writing the document title

--- opal/opal-jquery/document.rb
+++ opal/opal-jquery/document.rb
@@ -7,6 +7,14 @@ class << $document
   def ready?(&block)
     ::Element.ready?(&block)
   end
+
+  def title
+    `document.title`
+  end
+
+  def title=(title)
+    `document.title = #{title}`
+  end
 end
 
 # TODO: this will be removed soon (here for compatibility)
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
-describe "Document.ready?" do
-  it "accepts a block" do
-    $document.ready? { }
+describe Document do
+  describe "ready?" do
+    it "accepts a block" do
+      Document.ready? { }
+    end
+  end
+
+  describe "title" do
+    it "gets the document title" do
+      Document.title.should be_kind_of(String)
+    end
+  end
+
+  describe "title=" do
+    it "sets the document title" do
+      old = Document.title
+      Document.title = "foo"
+      Document.title.should eq("foo")
+      Document.title = old
+    end
   end
 end
