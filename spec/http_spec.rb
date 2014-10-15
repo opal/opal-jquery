78bf5acb1cb99b72768d9a6c6ae1f0c3c0a50aeb
Adam Beynon <adam.beynon@gmail.com>
2014-10-15 16:51:06 +0100

    Some cleanup

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe HTTP do
   describe ".setup" do
     it 'presents the $.ajaxSetup() object as a Hash' do
-      HTTP.setup.should be_a Hash
+      expect(HTTP.setup).to be_a Hash
     end
   end
 
@@ -16,13 +16,13 @@ describe HTTP do
 
       async "block gets called on success" do
         HTTP.get('spec/fixtures/simple.txt') do |response|
-          run_async { response.should be_ok }
+          async { response.should be_ok }
         end
       end
 
       async "block gets called on failure" do
         HTTP.get('/spec/does/not/exist.txt') do |response|
-          run_async { response.should_not be_ok }
+          async { response.should_not be_ok }
         end
       end
     end
@@ -34,13 +34,13 @@ describe HTTP do
 
       async "returns a promise which accepts a then-block for successful response" do
         HTTP.get('spec/fixtures/simple.txt').then do |response|
-          run_async { response.should be_ok }
+          async { response.should be_ok }
         end
       end
 
       async "returns a promise which accepts a fail-block for failing response" do
         HTTP.get('spec/does/not/exist.txt').fail do |response|
-          run_async { response.should_not be_ok }
+          async { response.should_not be_ok }
         end
       end
     end
@@ -49,7 +49,7 @@ describe HTTP do
   describe '#body' do
     async 'returns the response body as a string' do
       HTTP.get('spec/fixtures/simple.txt') do |response|
-        run_async { response.body.should == "hey" }
+        async { response.body.should == "hey" }
       end
     end
   end
@@ -57,7 +57,7 @@ describe HTTP do
   describe '#json' do
     async 'returns the json converted into native ruby objects' do
       HTTP.get('spec/fixtures/user.json') do |response|
-        run_async { response.json.should == { 'name' => 'Adam', 'age' => 26 } }
+        async { response.json.should == { 'name' => 'Adam', 'age' => 26 } }
       end
     end
   end
@@ -65,13 +65,13 @@ describe HTTP do
   describe '#ok?' do
     async 'returns true when the request was a sucess' do
       HTTP.get('spec/fixtures/simple.txt') do |response|
-        run_async { response.should be_ok }
+        async { expect(response).to be_ok }
       end
     end
 
     async 'returns false when the request failed' do
       HTTP.get('spec/fixtures/non_existant.txt') do |response|
-        run_async { response.should_not be_ok }
+        async { response.should_not be_ok }
       end
     end
   end
