95a9e840d4ed6bd54d509f558170b3e5d01c0a63
Adam Beynon <adam.beynon@gmail.com>
2014-11-01 14:59:06 +0000

    Some spec tidying

--- spec/element/after_spec.rb
+++ spec/element/after_spec.rb
@@ -1,6 +1,6 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe "Element#after" do
+describe 'Element#after' do
   html <<-HTML
     <div id="some-header" class="kapow"></div>
     <div id="foo" class="after-spec-first"></div>
@@ -8,18 +8,18 @@ describe "Element#after" do
     <div id="baz"></div>
   HTML
 
-  it "should insert the given html string after each element" do
-    el = Element.find('.after-spec-first')
-    el.size.should == 2
+  it 'should insert the given html string after each element' do
+    el = find '.after-spec-first'
+    expect(el.size).to eq(2)
 
     el.after '<p class="woosh"></p>'
 
-    Element.find('#foo').next.class_name.should == "woosh"
-    Element.find('#bar').next.class_name.should == "woosh"
+    expect(find('#foo').next.class_name).to eq('woosh')
+    expect(find('#bar').next.class_name).to eq('woosh')
   end
 
-  it "should insert the given DOM element after this element" do
-    Element.find('#baz').after Element.find('#some-header')
-    Element.find('#baz').next.id.should == "some-header"
+  it 'should insert the given DOM element after this element' do
+    find('#baz').after find('#some-header')
+    expect(find('#baz').next.id).to eq('some-header')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
 require 'opal-rspec'
 require 'opal-jquery'
 require 'opal-jquery/rspec'
+
+module JQueryTestHelpers
+  def find(selector)
+    Element.find selector
+  end
+end
+
+RSpec.configure do |config|
+  config.include JQueryTestHelpers
+end
