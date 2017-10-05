d7da79dd7652cad82dc56e888542932ba8f69f9f
Elia Schito <elia@schito.me>
2017-10-05 23:27:36 +0200

    Improve method_missing specs

--- spec/element/method_missing_spec.rb
+++ spec/element/method_missing_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe "Element#method_missing" do
       expect(Element['#foo'].offsetParent).to eq(Element['body'])
     end
   end
+
+  context 'jQuery plugin methods' do
+    subject(:element) { Element.new }
+
+    before do
+      `$.fn.pluginMethod = function() {return 123}`
+    end
+
+    after do
+      `delete $.fn.pluginMethod; delete $.fn.$pluginMethod;`
+    end
+
+    it 'calls method_missing' do
+      expect(element).to receive(:method_missing).once.with(:pluginMethod)
+      element.pluginMethod
+    end
+
+    it 'calls forwards to the plugin', :focus do
+      pending "broken on opal < 0.11" if RUBY_ENGINE_VERSION.to_f < 0.11
+      expect(element.pluginMethod).to eq(123)
+    end
+  end
 end
